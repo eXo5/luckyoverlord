8a8a88ae241ea4793b7045ef1e37e36098368805
Mike <Mike.yingling15@gmail.com>
2017-07-10 14:34:04 -0400

    working api for nutritionix

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,30 +1,48 @@
 //grab db definition for use in route handler.
 //var db = require("../models");
+var path = require("path");
+var $ = require("jquery");
+// ensure you are passing a string with queries delimited by new lines. 
 
 module.exports = function(app) {
+    var request = require('request');
 
-		app.get("/", function(req, res){
 
-				res.render("index", {things: "Stuff"});
-		
-		});
+    var NutritionixClient = require('nutritionix');
+    var nutritionix = new NutritionixClient({
+        appId: '5176df0a',
+        appKey: '7461830ebf151da56366f14acd51751e'
+            // debug: true, // defaults to false 
+    });
 
-		// app.post("/create", function(req, res){
-		// 	db.burgers.create({burger_name: req.body.burgerName})
-		// 	.then(res.redirect("/"));
-		// });
+    var ingredients = [
+        '1 tbsp sugar',
+        '1 red pepper'
+    ];
 
-		// app.put("/burger/eat/:burgId", function(req, res){//update function
-		// 	var id = req.params.burgId;
-		// 	db.burgers.update({//db.burgers.update calling the sequelize update function on the db.burgers table.
-		// 		devoured: true
-		// 	},
-		// 	{	
-		// 		where: {
-		// 			id: id
-		// 		}//sequelize knows that it's id key is not the var id lexically defined here.
-		// 	}).then(function(dbPut){
-		// 		res.redirect("/");
-		// 	});
-		// });
+    //get request "/" sends welcome.html
+    app.get("/", function(req, res) {
+        res.sendFile(path.resolve("public", "welcome.html"));
+        //path.resolve defines that root as absolute for sendFile
+    });
+
+    app.post("/newLord", function(req, res) {
+        console.log(req.body);
+    });
+
+    app.post("/food", function(req, res) {
+        var food = req.body.ingredient;
+        console.log("Food Search: " + food);
+        request("https://api.nutritionix.com/v1_1/search/" + food + "?results=0%3A20&cal_min=125&cal_max=50000&fields=*&appId=5176df0a&appKey=+7461830ebf151da56366f14acd51751e", function(error, response, body) {
+            console.log('error:', error); // Print the error if one occurred 
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+            console.log('body:', body); // Print the HTML for the Google homepage. 
+            console.log("nf_calories: " + body)
+            var parseBody = JSON.parse(body);
+            res.send(parseBody.hits[0].fields);
+
+
+        });
+    })
+
 }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+var path = require("path");
 
 var app = express();
 	app.use(methodOverride("_method"));
