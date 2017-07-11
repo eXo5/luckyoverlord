bec2ce6e0c2b4b1be05d679475e212d1a58fb193
eXo5 <mike.yingling15@gmail.com>
2017-07-10 22:52:39 -0400

    api works as defined in index.js and api-routes.js. Whole thing should come together soon

---
+++ models/meal.js
@@ -0,0 +1,62 @@
+module.exports = function(sequelize, DataTypes) {
+  sequelize.authenticate().then(function(errs){ if (errs){console.log("errors: " + errs)} });
+		var Meal = sequelize.define("Meal", {
+			meal_name: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
+			ingredient1: {
+				type: DataTypes.STRING	
+			},
+			ingredient2: {
+				type: DataTypes.STRING	
+			},
+			ingredient3: {
+				type: DataTypes.STRING	
+			},
+			ingredient4: {
+				type: DataTypes.STRING	
+			},
+			ingredient5: {
+				type: DataTypes.STRING	
+			},
+			ingredient6: {
+				type: DataTypes.STRING	
+			},
+			ingredient7: {
+				type: DataTypes.STRING	
+			},
+			ingredient8: {
+				type: DataTypes.STRING	
+			},
+			ingredient9: {
+				type: DataTypes.STRING	
+			},
+			ingredient10: {
+				type: DataTypes.STRING	
+			},
+			calories: {
+				type: DataTypes.DECIMAL
+			},
+			fat: {
+				type: DataTypes.DECIMAL
+			},
+			protein:{
+				type: DataTypes.DECIMAL
+			},
+			carbs:{
+				type: DataTypes.DECIMAL
+			}
+			},{
+				classMethods: {
+			        associate: function(models) {
+					Meal.belongsTo(models.User, {
+	           			 onDelete: "cascade"
+	          });
+	        }
+				},
+				timestamps: false
+			
+		});
+		return Meal
+	};
--- server.js
+++ server.js
@@ -17,14 +17,14 @@ var exphbs = require("express-handlebars");
 
 var PORT = process.env.PORT || 3000;
 
-//var db = require("./models");
+var db = require("./models");
 require("./routes/api-routes.js")(app);
 //require("./routes/html-routes.js")(app);
 
 
-//
-//db.sequelize.sync().then(function() {
+//Sync database, then start server.
+db.sequelize.sync().then(function() {
 	app.listen(PORT, function(){
 		console.log("Welcome to Machine " + PORT);
 	});
-//});
+});
